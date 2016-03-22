ae908e82e97fc631092bc0f82b1f6d63ffa11765
Michael Assraf <michael@atlis.me>
2016-03-22 11:28:34 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/com/atlis/actions/impl/UserFriendsActions.java
    	src/main/java/com/atlis/actions/impl/UserServiceProvidersActions.java

--- src/main/java/com/atlis/location/utils/LocationUtils.java
+++ src/main/java/com/atlis/location/utils/LocationUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.atlis.location.nominatim.utils;
+package com.atlis.location.utils;
 
 import com.atlis.location.model.impl.BoundingBox;
 import com.atlis.location.model.impl.MapPoint;
